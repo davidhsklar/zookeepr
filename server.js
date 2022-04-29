e4fd6792c8ee001759c53cf752eb717ea2eb3ad9
David Sklar <davidhsklar@gmail.com>
2022-04-29 13:00:03 -0400

    added post methods and tested with insomnia

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
-const { animals } = require('./api/animals.json');
+const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// parse (meaning decode) incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+
+// parse incoming json data
+app.use (express.json());
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -36,6 +45,17 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+      path.join(__dirname, './data/animals.json'),
+      JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    return animal;
+  }
+
+
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
@@ -44,7 +64,7 @@ app.get('/api/animals', (req, res) => {
   res.json(results);
 });
 
-app.get('/api/animals:id', (req, res) => {
+app.get('/api/animals/:id', (req, res) => {
   const result = findById(req.params.id, animals);
   if (result) {
     res.json(result);
@@ -52,6 +72,45 @@ app.get('/api/animals:id', (req, res) => {
     res.send(404);
   }
 });
+
+app.post('/api/animals', (req, res) => {
+
+    //sets id based on what the next index of the array will be
+    req.body.iq = animals.length.toString();
+
+    // if any data in req.body is incorrect, send error back
+    if (!validateAnimal(req.body)){
+        res.status(400).send('No thanks, try again.');
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+});
+
+    //add animal to json file and animals array in this function
+
+    const animal = createNewAnimal(req.body, animals);{
+
+    // req.body is where our incoming content will be
+    console.log(animal);
+    res.json(animal);
+  }
+
+  function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
